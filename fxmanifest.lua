6a8356d2f9aeb0e628d262b3e21dc856a2ff488a
Matt <31101519+Kronus8@users.noreply.github.com>
2023-10-11 21:54:06 +0100

    Add missing RDR3 Warning to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'cerulean'
-
 games {"gta5", "rdr3"}
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-author "Project Error"
+author ""
 version '1.0.0'
 
 lua54 'yes'
